3bb1b1de464ff9f92380ac702a5928ebc7c8ee71
Sol Vallejos <sol.vallejos65@gmail.com>
2022-03-18 19:25:48 -0300

    agregando cambios

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* TODO */
+
+/* <p>Calcular e informar un sueldo sabiendo que:</p>
+      <p>- La categoría define el sueldo básico mensual (1: u$s 1000, 2: u$s 1500, 3: u$s 2000).</p>
+      <p>- La subcategoría define el multiplicador (A: x1.5, B: x2, C: x2.5).</p>
+      <p>- Ambos datos son ingresados por el usuario.</p>
+      <p>Además, informar dentro de qué rango está el sueldo:</p>
+      <p>- Si está en el rango ‘inicial’: entre u$s 1500 a u$s 3000, inclusive.</p>
+      <p>- Si está en el rango ‘intermedio’: entre u$s 3001 y u$s 4000, inclusive.</p>
+      <p>- Si está en el rango ‘avanzado’: si supera los u$s 4000.</p>*/
+
+let categoria = +prompt('Ingrese la categoria, 1,2 o 3');
